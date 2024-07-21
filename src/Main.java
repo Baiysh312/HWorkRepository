54d783e619d7e63a825eebb531c01b931f7f8dff
Baiysh Mamatov <altynbekovich.98@gmail.com>
2024-07-21 21:38:52 +0600

    poka bez izmenenii

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String git;
+        Scanner scan= new Scanner(System.in);
+        git = scan.next();
+        if (git=="git"){
+            System.out.println("this is git");
+        }
+    }
+}
